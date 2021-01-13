bf473dd46ab875d9666160745bb495731f6cd418
hugo <cythbgy@gmail.com>
2021-01-14 02:03:01 +0800

    jssdk基本架构完成

--- src/index.js
+++ src/index.js
@@ -1,4 +1,23 @@
+
+/**
+ *
+ *
+ * @param {*} text
+ */
+function log(text) {
+    console.log(text);
+    document.getElementById('output').innerHTML = text;
+}
+
+/** Class representing a point. */
 class Native {
+    constructor() {
+        // 保存需要长期存在的回调
+        this._cbs = {}
+        // 通过eventId来与iOS关联，一旦ios执行完成，通过eventId来查找回调方法。找到方法之后运行并删除event。
+        this._events = {}
+    }
+
     /**
      *
      *
@@ -10,5 +29,103 @@ class Native {
         document.getElementById('output').innerHTML = 'hello';
         return 'hello';
     }
+
+    /**
+     * 注册监听事件, 事件完成之后会调用`callback`方法。
+     * 
+     * 回调时会传入text参数
+     *
+     * @param {function} callback 
+     * @param {string} text
+     * @memberof Native
+     */
+    onTest(callback) {
+        this._cbs['onTest'] = callback
+    }
+
+    /**
+     * 取消注册监听事件
+     *
+     * @memberof Native
+     */
+    offTest() {
+        f = this._cbs.onTest
+        if (f == undefined) {
+            return
+        }
+        delete this._cbs.onTest
+    }
+
+    uuid() {
+        var s = [];
+        var hexDigits = "0123456789abcdef";
+        for (var i = 0; i < 36; i++) {
+            s[i] = hexDigits.substr(Math.floor(Math.random() * 0x10), 1);
+        }
+        s[14] = "4";  // bits 12-15 of the time_hi_and_version field to 0010
+        s[19] = hexDigits.substr((s[19] & 0x3) | 0x8, 1);  // bits 6-7 of the clock_seq_hi_and_reserved to 01
+        s[8] = s[13] = s[18] = s[23] = "-";
+
+        var uuid = s.join("");
+        return uuid;
+    }
+
+    /**
+     * ios执行完成之后会调用该方法来执行对应事件的回调
+     *
+     * @param {string} eventId
+     * @param {bool} isSuccess
+     * @param {object} data
+     * @memberof Native
+     */
+    done(eventId, isSuccess, data) {
+        k = this._events.eventId
+        if (k == undefined) {
+            return
+        }
+
+        if (isSuccess) {
+            k.success(data)
+        } else {
+            k.fail(data)
+        }
+        k.complete()
+
+        delete this._events.eventId
+    }
+
+    /**
+     * 关闭所有页面，打开到应用内的某个页面
+     *
+     * @param {string} url 需要跳转的应用内页面路径 (代码包路径)，路径后可以带参数。参数与路径之间使用?分隔，参数键与参数值用=相连，不同参数用&分隔；如 'path?key=value&key2=value2'
+     * @param {function} success(object) 接口调用成功的回调函数。
+     * @param {function} fail(object) 接口调用失败的回调函数
+     * @param {function} complete 接口调用结束的回调函数（调用成功、失败都会执行）
+     * @memberof Native
+     */
+    reLaunch(url, success, fail, complete) {
+        eventId = this.uuid()
+        this._events.eventId = {
+            'success': success,
+            'fail': fail,
+            'complete': complete
+        }
+
+        msg = {url: url, eventId: eventId}
+        window.webkit.messageHandlers.reLaunch.postMessage(msg);
+    }
 }
 const native = new Native();
+
+// native.onTest( (data) => {
+//     log(data)
+// })
+
+// setInterval(function() {
+//     f = native._cbs.onTest
+//     if (f == undefined) {
+//         return
+//     }
+//     native._cbs.onTest('test onText')
+//     native.offTest()
+// }, 3000)
